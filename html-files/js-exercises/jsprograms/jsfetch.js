bdf900fc28a19c9c5963144aec97a4174b80f400
analyzecode <107164771+analyzecode@users.noreply.github.com>
2022-06-12 12:45:36 +0530

    js programs uploaded
    ...

---
+++ html-files/js-exercises/jsprograms/jsfetch.js
@@ -0,0 +1,98 @@
+const jscode1 = document.querySelector(".jscode1");
+const jscode2 = document.querySelector(".jscode2");
+const jscode3 = document.querySelector(".jscode3");
+const jscode4 = document.querySelector(".jscode4");
+const jscode5 = document.querySelector(".jscode5");
+const jscode6 = document.querySelector(".jscode6");
+const jscode7= document.querySelector(".jscode7");
+const jscode8 = document.querySelector(".jscode8");
+const jscode9 = document.querySelector(".jscode9");
+const jscode10 = document.querySelector(".jscode10");
+const jscode11 = document.querySelector(".jscode11");
+const jscode12 = document.querySelector(".jscode12");
+
+
+// csscode1
+fetch('js-txtfiles/js-Program1.html')
+.then(res => res.text())
+.then(data=>{
+  jscode1.innerText = data
+});
+
+
+// csscode2
+fetch('js-txtfiles/js-Program2.html')
+.then(res => res.text())
+.then(data=>{
+  jscode2.innerText = data
+});
+
+// csscode3
+fetch('js-txtfiles/js-Program3.html')
+.then(res => res.text())
+.then(data=>{
+  jscode3.innerText = data
+});
+
+// csscode4
+fetch('js-txtfiles/js-Program4.html')
+.then(res => res.text())
+.then(data=>{
+  jscode4.innerText = data
+});
+
+// csscode5
+fetch('js-txtfiles/js-Program5.html')
+.then(res => res.text())
+.then(data=>{
+  jscode5.innerText = data
+});
+
+// csscode6
+fetch('js-txtfiles/js-Program6.html')
+.then(res => res.text())
+.then(data=>{
+  jscode6.innerText = data
+});
+
+// csscode7
+fetch('js-txtfiles/js-Program7.html')
+.then(res => res.text())
+.then(data=>{
+  jscode7.innerText = data
+});
+
+// csscode8
+fetch('js-txtfiles/js-Program8.html')
+.then(res => res.text())
+.then(data=>{
+  jscode8.innerText = data
+});
+
+// csscode9
+fetch('js-txtfiles/js-Program9.html')
+.then(res => res.text())
+.then(data=>{
+  jscode9.innerText = data
+});
+
+// csscode10
+fetch('js-txtfiles/js-Program10.html')
+.then(res => res.text())
+.then(data=>{
+  jscode10.innerText = data
+});
+
+// csscode11
+fetch('js-txtfiles/js-Program11.html')
+.then(res => res.text())
+.then(data=>{
+  jscode11.innerText = data
+});
+
+// csscode12
+fetch('js-txtfiles/js-Program12.html')
+.then(res => res.text())
+.then(data=>{
+  jscode12.innerText = data
+});
